fea088ef4baa24a3f9597cfadaf56de954fdba8c
jdguerrero28 <jdiegogueal@gmail.com>
2025-12-17 12:37:43 -0500

    Arreglos bidimensionales3

---
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Ejercicio {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        
+        String[] empleados = new String [5];
+        double[][] produccion = new double [5][12];
+        double [] totales = new double [5];
+        String lista = "";
+        double suma = 0;
+        
+        for (int i = 0; i<produccion.length;i++){
+        
+        
+        }
+    }
+}
